c84b326f91c9a56d6a5345c4842ac06cacc5ff33
Tai <107045644+TaiDmc@users.noreply.github.com>
2022-08-22 17:06:12 -0500

    xd

--- Ejercicios-JS/ajax/assets/stripe_keys.js
+++ Ejercicios-JS/ajax/assets/stripe_keys.js
@@ -1,6 +1,6 @@
 export default {
   public:
-    "pk_test_51LXDDgIHyzQYF2d5DY26ZRQclrAOrAQC4K19bt1BeMM7hwKibgKFXstfJP00N394hd8z7NtftYrqPGrvpXhwu8DE00E4RToM8l",
+    "Your Public key",
   secret:
-    "sk_test_51LXDDgIHyzQYF2d5iv1oggoKcMG38uRTAUX12GdZIDpKiRcqT2hIiHSlwo3ARoe3fo5xi5xjRQNGX1ivrQM1MgVg00AiDkHGhB",
+    "Your Secret key",
 };
